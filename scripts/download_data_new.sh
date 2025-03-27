c651d2f9280c16fb8a791fb9446f2132f95e4254
Ggomezdemkura <gabriel.gomezdemkura@uzh.ch>
2025-03-27 10:40:03 +0100

    Functional Training and generation with dune 1-6

--- scripts/download_data_new.sh
+++ scripts/download_data_new.sh
@@ -20,19 +20,19 @@ mkdir -p $data/dune/raw
 #wget https://www.gutenberg.org/files/52521/52521-0.txt
 #mv 52521-0.txt $data/grimm/raw/tales.txt
 
-cp /dune.txt $data/dune/raw/dune.txt
+cp /dune_full.txt $data/dune/raw/dune_full.txt
 
 # preprocess slightly
 
-cat $data/dune/raw/dune.txt | python $base/scripts/preprocess_raw.py > $data/dune/raw/dune.cleaned.txt
+cat $data/dune/raw/dune_full.txt | python $base/scripts/preprocess_raw.py > $data/dune/raw/dune_full.cleaned.txt
 
 # tokenize, fix vocabulary upper bound
 
-cat $data/dune/raw/dune.cleaned.txt | python $base/scripts/preprocess.py --vocab-size 5000 --tokenize --lang "en" --sent-tokenize > \
-    $data/dune/raw/dune.preprocessed.txt
+cat $data/dune/raw/dune_full.cleaned.txt | python $base/scripts/preprocess.py --vocab-size 10000 --tokenize --lang "en" --sent-tokenize > \
+    $data/dune/raw/dune_full.preprocessed.txt
 
 # split into train, valid and test
 
-head -n 440 $data/dune/raw/dune.preprocessed.txt | tail -n 400 > $data/dune/valid.txt
-head -n 840 $data/dune/raw/dune.preprocessed.txt | tail -n 400 > $data/dune/test.txt
-tail -n 3075 $data/dune/raw/dune.preprocessed.txt | head -n 2955 > $data/dune/train.txt
+head -n 440 $data/dune/raw/dune_full.preprocessed.txt | tail -n 400 > $data/dune/valid.txt
+head -n 840 $data/dune/raw/dune_full.preprocessed.txt | tail -n 400 > $data/dune/test.txt
+tail -n 3075 $data/dune/raw/dune_full.preprocessed.txt | head -n 2955 > $data/dune/train.txt
